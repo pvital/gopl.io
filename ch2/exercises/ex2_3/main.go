4d87d0c1964328b8fd7fff687a06ad4103a1f18b
Paulo Vital <paulo.vital@perseus.de>
2021-08-23 22:32:48 +0200

    Add exercise 3 from Chapter 2

---
+++ ch2/exercises/ex2_3/main.go
@@ -0,0 +1,28 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 45.
+
+package popcount
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+
+	sum = 0 
+	for i := range 7 {
+		sum += pc[byte(x>>(i*8))]
+	}
+
+	return int(sum)
+}
+
+//!-
